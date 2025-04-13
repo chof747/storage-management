65076128238ad12e3ab7db5d2b74646c010e993c
Christian Hofbauer <chof@gmx.at>
2025-04-13 22:52:29 +0200

    fixed issue with models

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -4,7 +4,6 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-import os
 from dotenv import load_dotenv
 
 # Import your actual models and Base
@@ -12,6 +11,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app.database import Base  # adjust if needed
+from app.models import hardware_item 
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -35,6 +35,7 @@ config.set_main_option("sqlalchemy.url", get_url())
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
