18a5e5e352a664415b70634e9c69edf086dc09e5
Christian Hofbauer <chof@gmx.at>
2025-05-11 14:01:14 +0200

    implemented move api function for hardwareitems

--- backend/app/api/hardware_item.py
+++ backend/app/api/hardware_item.py
@@ -3,11 +3,18 @@ from sqlalchemy.orm import Session
 
 from app.database import SessionLocal
 from app.models.hardware_item import HardwareItem
-from app.schemas.hardware_item import HardwareItemCreate, HardwareItemUpdate, HardwareItemInDB, HardwareItemPage
+from app.schemas.hardware_item import (
+    HardwareItemCreate,
+    HardwareItemUpdate,
+    HardwareItemInDB,
+    HardwareItemPage,
+    HardwareItemsMoveRequest,
+)
 from .pagination import page_parameters
 
 
 router = APIRouter(prefix="/api/items", tags=["Hardware Items"])
+
 
 def get_db():
     db = SessionLocal()
@@ -16,30 +23,22 @@ def get_db():
     finally:
         db.close()
 
+
 @router.get("/", response_model=HardwareItemPage)
-def list_items(
-    pagination = Depends(page_parameters),
-    db: Session = Depends(get_db)):
+def list_items(pagination=Depends(page_parameters), db: Session = Depends(get_db)):
 
     total = db.query(HardwareItem).count()
-    return {
-        "total": total,
-        "items" : pagination(db.query(HardwareItem)).all()
-    }
+    return {"total": total, "items": pagination(db.query(HardwareItem)).all()}
+
 
 @router.get("/bystorage", response_model=HardwareItemPage)
 def list_items_bystorage(
-    storage: int,
-    pagination = Depends(page_parameters),
-    db: Session = Depends(get_db)):    
+    storage: int, pagination=Depends(page_parameters), db: Session = Depends(get_db)
+):
 
     q = db.query(HardwareItem).filter(HardwareItem.storage_element_id == storage)
-    return {
-        "total": q.count(),
-        "items": pagination(q).all()
-    }
+    return {"total": q.count(), "items": pagination(q).all()}
 
-        
 
 @router.post("/", response_model=HardwareItemInDB)
 def create_item(item: HardwareItemCreate, db: Session = Depends(get_db)):
@@ -48,6 +47,7 @@ def create_item(item: HardwareItemCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(db_item)
     return db_item
+
 
 @router.put("/{item_id}", response_model=HardwareItemInDB)
 def update_item(item_id: int, item: HardwareItemUpdate, db: Session = Depends(get_db)):
@@ -59,6 +59,7 @@ def update_item(item_id: int, item: HardwareItemUpdate, db: Session = Depends(ge
     db.commit()
     return db_item
 
+
 @router.delete("/{item_id}")
 def delete_item(item_id: int, db: Session = Depends(get_db)):
     db_item = db.query(HardwareItem).get(item_id)
@@ -68,20 +69,35 @@ def delete_item(item_id: int, db: Session = Depends(get_db)):
     db.commit()
     return {"message": "Deleted"}
 
+
 @router.get("/queueforprinting/{item_id}")
 def queue_for_printing(item_id: int, db: Session = Depends(get_db)):
-    hw_item : HardwareItem = db.query(HardwareItem).get(item_id)
+    hw_item: HardwareItem = db.query(HardwareItem).get(item_id)
     if not hw_item:
         raise HTTPException(status_code=404, detail="Item not found")
     setattr(hw_item, "queued_for_printing", 1)
     db.commit()
     return {"message": f"item {item_id} queued for label printing"}
 
+
 @router.get("/unqueueforprinting/{item_id}")
 def queue_for_printing(item_id: int, db: Session = Depends(get_db)):
-    hw_item : HardwareItem = db.query(HardwareItem).get(item_id)
+    hw_item: HardwareItem = db.query(HardwareItem).get(item_id)
     if not hw_item:
         raise HTTPException(status_code=404, detail="Item not found")
     setattr(hw_item, "queued_for_printing", 0)
     db.commit()
     return {"message": f"item {item_id} queued for label printing"}
+
+
+@router.post("/move")
+def move_items(request: HardwareItemsMoveRequest, db: Session = Depends(get_db)):
+
+    for item_id in request.items:
+        hw_item: HardwareItem = db.query(HardwareItem).get(item_id)
+        if not hw_item:
+            raise HTTPException(status_code=404, detail=f"Item {item_id} not found")
+        setattr(hw_item, "storage_element_id", request.storage_to)
+
+    db.commit()
+    return {"message": f"Moved {len(request.items)} items to {request.storage_to}"}
--- backend/app/schemas/hardware_item.py
+++ backend/app/schemas/hardware_item.py
@@ -1,12 +1,16 @@
-
 from pydantic import BaseModel, Field, field_validator
 from typing import Optional, List
 from app.types import StrHttpUrl
 from .storage_element import StorageElementInDB
 
+
 class HardwareItemBase(BaseModel):
-    hwtype: str = Field(..., min_length=2, max_length=80, description="Type of hardware")
-    main_metric: str = Field(..., min_length=2, max_length=30, description="Main metric")
+    hwtype: str = Field(
+        ..., min_length=2, max_length=80, description="Type of hardware"
+    )
+    main_metric: str = Field(
+        ..., min_length=2, max_length=30, description="Main metric"
+    )
     secondary_metric: Optional[str] = None
     length: Optional[float] = None
     reorder: Optional[bool] = False
@@ -14,26 +18,34 @@ class HardwareItemBase(BaseModel):
     storage_element_id: int = Field(..., description="Link to storage element")
     queued_for_printing: Optional[bool] = False
 
-
     @field_validator("main_metric")
     def must_start_with_letter(cls, v: str):
         if not v or not v[0].isalpha():
             raise ValueError("Metric one must start with a letter")
         return v
-    
+
+
 class HardwareItemCreate(HardwareItemBase):
     pass
+
 
 class HardwareItemUpdate(HardwareItemBase):
     pass
 
+
 class HardwareItemInDB(HardwareItemBase):
     id: int
-    storage_element : StorageElementInDB
+    storage_element: StorageElementInDB
+
     class Config:
         from_attributes = True
+
 
 class HardwareItemPage(BaseModel):
     items: List[HardwareItemInDB]
     total: int
 
+
+class HardwareItemsMoveRequest(BaseModel):
+    storage_to: int
+    items: List[int]
