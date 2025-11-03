b0797d3bc26fac57a19534ee626ec6b71992fc81
Christian Hofbauer <chof@gmx.at>
2025-11-03 06:57:36 +0100

    moved build filter parameters to common.tsx

--- frontend/src/api/electronicParts.tsx
+++ frontend/src/api/electronicParts.tsx
@@ -1,16 +1,14 @@
 import { ResultPage, QueryFilter } from "../types/page";
 import { getApiBase } from "./endpoint";
 import { PartsBoxItem } from "../types/partsboxItems";
+import { buildFilterParams } from "./common";
 
 const PATH = "electronic-parts";
 
 export const getItems = async (offset: number, limit: number, filters: QueryFilter[] = []): Promise<ResultPage<PartsBoxItem>> => {
   const API_BASE = await getApiBase();
 
-  var filterParams = filters.map(f => `${encodeURIComponent(f.key)}:${encodeURIComponent(f.value)}`).join('&filter=');
-  if (filterParams) {
-    filterParams = '&filter=' + filterParams;
-  }
+  const filterParams = buildFilterParams(filters);
   const res = await fetch(`${API_BASE}/${PATH}/?offset=${offset}&limit=${limit}${filterParams}`);
   return await res.json();
 };
