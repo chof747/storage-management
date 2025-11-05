2e0f6e45d8545a6fca4528f7bb0ee05574159cdf
Christian Hofbauer <chof@gmx.at>
2025-11-05 23:14:43 +0100

    test: update file to better demonstrate hooks

--- backend/app/test_hooks.py
+++ backend/app/test_hooks.py
@@ -1,3 +1,5 @@
 def test_function():    
-    x=1   
+    x=1   # badly formatted assignment
+    y= [1,2,   3]  # bad spacing in list
+    print ('debug')  # flake8 should warn about print
     return x  
